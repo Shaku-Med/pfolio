d0fa3157d47ccf131884e1192cce1960a4d0d9bd
Magendaam <amaramagenda18@gmail.com>
2024-07-08 06:42:58 -0400

    s

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -6,17 +6,17 @@ export const metadata: Metadata = {
     title: `About Me || Mohamed Amara`,
     description: `Hey there! I'm Mohamed, and I love creating stuff that lives on devices and the internet. My journey into software development kicked off in 2019 during the COVID-19 pandemic. I began with Python and, over time, explored various areas, getting hooked on programming. This curiosity led me to dive into app development, web development, and machine learning.`,
     openGraph: {
-        description: "Hey there! I'm Mohamed, and I love creating stuff that lives on devices and the internet. My journey into software development kicked off in 2019 during the COVID-19 pandemic. I began with Python and, over time, explored various areas, getting hooked on programming. This curiosity led me to dive into app development, web development, and machine learning.",
+        description: `Hey there! I'm Mohamed, and I love creating stuff that lives on devices and the internet. My journey into software development kicked off in 2019 during the COVID-19 pandemic. I began with Python and, over time, explored various areas, getting hooked on programming. This curiosity led me to dive into app development, web development, and machine learning.`,
         images: [
             {
                 url: `https://medzyamara.com/me1.jpg`,
             }
         ],
-        title: "Medzy Amara | Portfolio",
+        title: `Medzy Amara | Portfolio`,
     },
     twitter: {
-        site: "@medzyamara",
-        card: "summary_large_image",
+        site: `@medzyamara`,
+        card: `summary_large_image`,
         images: [
             {
                 url: `https://medzyamara.com/me1.jpg`,
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
 const page = () => {
     return (
         <>
-            <div className="clsmbd fixed top-0 left-0 overflow-auto w-full h-full ">
+            <div className="clsmbd fixed top-0 left-0 overflow-auto w-full h-full">
                 <About />
             </div>
         </>
