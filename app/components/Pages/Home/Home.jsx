2de5d35c598ae1e4c23510e76c70bf1a0e504c95
Medzy Ems Amara <100793520+Shaku-Med@users.noreply.github.com>
2025-03-20 05:50:14 +0000

    s

--- app/components/Pages/Home/Home.jsx
+++ app/components/Pages/Home/Home.jsx
@@ -39,6 +39,18 @@ export default async function Home() {
                     <div id='contact' className="aidfnoad snap-start ">
                         <Contact sid={uid} h={h} />
                     </div>
+                    <div className='aidfnoad snap-start'>
+                    <iframe 
+                        style={{ borderRadius: '12px' }} 
+                        src="https://open.spotify.com/embed/artist/0n7maaPRkmcz9CEJupVCT1?utm_source=generator" 
+                        width="100%" 
+                        height="152" 
+                        frameBorder="0" 
+                        allowFullScreen 
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                        loading="lazy" 
+                        />
+                    </div>
                     <div id='footer' className="aidfnoad snap-start ">
                         <Footer />
                     </div>
