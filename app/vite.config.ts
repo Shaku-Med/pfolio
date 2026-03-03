969deb62586bea4f0bdd0b0f83546f3b11280415
Shaku-Med <jujubelt124@gmail.com>
2026-03-02 19:39:18 -0500

    The allowed host was moved inside server

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,15 +6,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ mode }) => {
   loadEnv(mode, process.cwd(), "");
   let isDev = mode === "development";
-  console.log("is dev: ", isDev);
-  console.log("port: ", !isDev ? 3001 : 3000);
   return {
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     server: {
       port: !isDev ? 3001 : 3000,
       host: true,
+      allowedHosts: ["medzy.brozy.org"],
+      cors: true,
     },
-    allowedHosts: ["medzy.brozy.org"],
-    cors: true,
   };
 });
