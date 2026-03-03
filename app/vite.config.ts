cd0c88efcd86e282e30353d3901e0bb0c3caae3f
Shaku-Med <jujubelt124@gmail.com>
2026-03-02 19:38:03 -0500

    Debugging

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,6 +6,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ mode }) => {
   loadEnv(mode, process.cwd(), "");
   let isDev = mode === "development";
+  console.log("is dev: ", isDev);
+  console.log("port: ", !isDev ? 3001 : 3000);
   return {
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     server: {
